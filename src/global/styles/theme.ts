44fdcfa6f23f488e25b66e5a90e04fbe3b6b1724
Mateus <mateus.botelho.pereira@gmail.com>
2022-07-11 03:20:41 -0300

    Icons and colors

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,6 +1,54 @@
 export const theme = {
-  colors: {
-
+  postItColors: {
+    rioDeJaneiro:{
+      primeiro: '#FBAD4B',
+      segundo: '#EE68A3',
+      terceiro: '#02ADE1',
+      quarto: '#E2E647',
+      quinto: '#FFD81B',
+    },
+    miami:{
+      primeiro: '#70CBC2',
+      segundo: '#E2E647',
+      terceiro: '#F3778F',
+      quarto: '#F49EBB',
+      quinto: '#FBAD4B',
+    },
+    marrakesh:{
+      primeiro: '#E75337',
+      segundo: '#FED919',
+      terceiro: '#DCDA47',
+      quarto: '#7BA6D1',
+      quinto: '#DB73A4',
+    },
+    boraBora:{
+      primeiro: '#DDDE45',
+      segundo: '#10B1AC',
+      terceiro: '#D0E4D9',
+      quarto: '#EDF7F6',
+      quinto: '#0EAEE2',
+    },
+    bali:{
+      primeiro: '#B0CDEB',
+      segundo: '#7CA5D1',
+      terceiro: '#FAB8A0',
+      quarto: '#DB73A4',
+      quinto: '#B9B6CB',
+    },
+    capeTown:{
+      primeiro: '#F3778F',
+      segundo: '#FBAD4B',
+      terceiro: '#71CAC4',
+      quarto: '#EE68A5',
+      quinto: '#E2E647',
+    },
+    jaipur:{
+      primeiro: '#C4E069',
+      segundo: '#F6D83C',
+      terceiro: '#E7AD4A',
+      quarto: '#C472A0',
+      quinto: '#90C9C2',
+    },
   },
 
   fonts: {
